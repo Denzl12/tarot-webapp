f12822dc3936569294dd927afc260bae8a6c3291
Denzl12 <danilboldirev350@gmail.com>
2026-02-02 01:33:01 +0300

    Add tarotDeck array with card names and images

---
+++ js ./deck.js
@@ -0,0 +1,15 @@
+const tarotDeck = [
+    { name: "Шут", img: "cards/major/fool.jpg" },
+    { name: "Маг", img: "cards/major/magician.jpg" },
+    { name: "Верховная Жрица", img: "cards/major/priestess.jpg" },
+    { name: "Императрица", img: "cards/major/empress.jpg" },
+    { name: "Император", img: "cards/major/emperor.jpg" },
+    { name: "Солнце", img: "cards/major/sun.jpg" },
+    { name: "Луна", img: "cards/major/moon.jpg" },
+    { name: "Мир", img: "cards/major/world.jpg" },
+
+    { name: "Туз Жезлов", img: "cards/wands/ace.jpg" },
+    { name: "Король Кубков", img: "cards/cups/king.jpg" },
+    { name: "Тройка Мечей", img: "cards/swords/three.jpg" },
+    { name: "Десятка Пентаклей", img: "cards/pentacles/ten.jpg" }
+];
